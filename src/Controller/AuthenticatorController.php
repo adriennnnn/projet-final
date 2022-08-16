2325491eb576a62d3a6188a5dc9ab3ba03c20180
adriennnnn <95212982+adriennnnn@users.noreply.github.com>
2022-08-16 18:16:58 +0200

    style page index init

--- src/Controller/AuthenticatorController.php
+++ src/Controller/AuthenticatorController.php
@@ -30,7 +30,9 @@ class AuthenticatorController extends AbstractController
      * @Route("/logout", name="app_logout")
      */
     public function logout(): void
-    {
+    {   
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        // return $this->render(); 
+        
     }
 }
