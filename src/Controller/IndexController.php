882d55663d8fb161feb4421c570b7f33c63dd8ae
adriennnnn <95212982+adriennnnn@users.noreply.github.com>
2022-11-07 10:20:42 +0100

    pre data transformer

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,20 +2,35 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\ShowCase;
+use App\Repository\UserRepository;
+use App\Repository\ShowCaseRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IndexController extends AbstractController
 {
     /**
      * @Route("/", name="app_homepage")
      */
-    public function index(): Response
+    public function index(ShowCaseRepository $showCaseRepository, UserRepository $userRepository): Response
     {
+        // $id = $this->getUser();
+        // $user = $userRepository->findOneBy(['id' => $id]);
+        // $showcaseByUser = $user->getShowCases();
+    
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
             // penser a ajouter les render pour pouvoir afficher les info des company et showcase
+
         ]);
+        // return $this->render('show_case/index.html.twig', [
+        //     'show_cases' => $showCaseRepository->findAll(),
+        //     'show_cases' => $showcaseByUser,
+    
+        // ]);
     }
+    
+
 }
